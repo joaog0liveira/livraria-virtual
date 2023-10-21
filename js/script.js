62f49bca8fcd75293c887b6a96d76cbb9563acd6
João Gabriel <118997962+joaog0liveira@users.noreply.github.com>
2023-10-21 10:18:15 -0300

    oc

---
+++ js/script.js
@@ -0,0 +1,38 @@
+const menuHamburguer = document.getElementById("menu-hamburguer");
+
+menuHamburguer.addEventListener("click", function() {
+  menuHamburguer.classList.toggle('active');
+});
+
+
+
+
+const imagem = document.getElementById("btn-busca");
+const headerForm = document.querySelector('.header-form');
+
+imagem.addEventListener("click", function() {
+    headerForm.classList.toggle('active');
+});
+
+
+window.onscroll = () => {
+
+    headerForm.classList.remove('active');
+
+    if(window.scrollY > 80){
+        document.querySelector('.header .header-2').classList.add('active');
+    } else {
+        document.querySelector('.header .header-2').classList.remove('active');
+    }
+
+}
+
+window.onload = () => {
+
+    if(window.scrollY > 80){
+        document.querySelector('.header .header-2').classList.add('active');
+    } else {
+        document.querySelector('.header .header-2').classList.remove('active');
+    }
+
+}
